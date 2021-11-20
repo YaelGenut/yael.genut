6fec3b4aabaca29c75c92aa4b1dd4072fc5c9f11
Yael <yaelgenut24@gmail.com>
2021-11-20 12:40:25 +0200

    message

---
+++ animation1.js
@@ -0,0 +1,20 @@
+function thankyou(){
+document.getElementById("button").innerHTML = "My name is Yael";
+console.log("My name is Yael")
+window.alert("הוזן בהצלחה")
+}
+
+var x;
+var y;
+
+x=5;
+y=7;
+
+var text = (x==y) ; 
+console.log(text);
+
+function changePic() {
+    document.getElementById("Ani").src="imgs/282780_904.jpg" 
+    ;
+}
+
